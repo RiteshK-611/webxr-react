eef66a8ad8b3de63e37b733c2ea5465c8c760f08
Ritesh <riteshkokam@gmail.com>
2021-09-16 15:42:37 +0530

    qr code added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import './styles.css';
 import '@google/model-viewer/dist/model-viewer.min.js';
 import hand from './assets/hand.png';
+import QRCode from 'qrcode.react'
 
 function App() {
   const modelViewer = {
@@ -15,22 +16,57 @@ function App() {
     marginLeft: 40,
   }
 
-  return (
-    <div className={App}>
-      <model-viewer className="modelviewer" style={modelViewer} src="https://modelviewer.dev/shared-assets/models/Astronaut.glb"
-        ios-src="https://modelviewer.dev/shared-assets/models/Astronaut.usdz"
-        alt="A 3D model of an astronaut"
-        ar
-        auto-rotate
-        camera-controls> 
+  if( navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+  ) 
+  {
+    return (
+      <div className={App}>
+        <model-viewer className="modelviewer" style={modelViewer} src="https://modelviewer.dev/shared-assets/models/Astronaut.glb"
+          ios-src="https://modelviewer.dev/shared-assets/models/Astronaut.usdz"
+          alt="A 3D model of an astronaut"
+          ar
+          auto-rotate
+          camera-controls> 
 
-        <button slot="ar-button" className="arbutton">
-          View in your space
-        </button>
+          <button slot="ar-button" className="arbutton">
+            View in your space
+          </button>
+        </model-viewer> 
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className={App}>
+        <model-viewer className="modelviewer" style={modelViewer} src="https://modelviewer.dev/shared-assets/models/Astronaut.glb"
+          ios-src="https://modelviewer.dev/shared-assets/models/Astronaut.usdz"
+          alt="A 3D model of an astronaut"
+          ar
+          auto-rotate
+          camera-controls> 
+        </model-viewer>
+        <div style={{display: 'flex', marginLeft: 50}}>
+          <QRCode 
+            id="1234"
+            value={window.location.href}
+            size={128}
+            bgColor={'#ffffff'}
+            fgColor={'#000000'}
+            level={'H'}
+            includeMargin={true}
+          />
+          <h5 style={{marginTop: 50}}>QRCode URL: {window.location.href}</h5>
+        </div>
+      </div>
+    );
+  }
 
-      </model-viewer>
-    </div>
-  );
 }
 
 export default App;
